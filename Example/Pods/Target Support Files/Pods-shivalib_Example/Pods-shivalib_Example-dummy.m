c2f30b11cdfdb2fd0897971b887d636e5526e965
UltivicShashi <Sushil.rana@ultivic.com>
2023-05-01 17:05:02 +0530

    Adding all files

---
+++ Example/Pods/Target Support Files/Pods-shivalib_Example/Pods-shivalib_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_shivalib_Example : NSObject
+@end
+@implementation PodsDummy_Pods_shivalib_Example
+@end
---
+++ Example/Pods/Target Support Files/shivalib/shivalib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_shivalib : NSObject
+@end
+@implementation PodsDummy_shivalib
+@end
