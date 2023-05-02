5188428cdbc64236f3597d1cfb52d8684050f4f3
Sushil <sushil.rana@ultivic.com>
2023-05-02 18:31:01 +0530

    Adding new tag

--- shiva_lib/Package.swift
+++ shiva_lib/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             name: "shiva_lib",
             targets: ["shiva_lib"]),
     ],
-    dependencies: [.package(url: "https://github.com/UltivicShashi/shivalib.git", from: "0.0.2")],
+    dependencies: [.package(url: "https://github.com/UltivicShashi/shivalib.git", from: "1.0.0")],
     targets: [
         .target(
             name: "shiva_lib",
